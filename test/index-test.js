6c440067aba3dc94cb2319ba1e98f5302837d2e4
Luke Ghenco <lukeghenco@gmail.com>
2017-06-06 11:31:25 -0400

    Update test to use chai

--- test/index-test.js
+++ test/index-test.js
@@ -1,36 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
-import { spyOn } from 'expect';
 
-import Game from '../components/Game';
-import Pancake from '../components/Pancake';
+import Game from '../src/components/Game';
+import Pancake from '../src/components/Pancake';
 
 describe('Game', () => {
   it('sets the initial time when the game was started in componentWillMount', () => {
-    const spy = expect.spyOn(Game.prototype, 'setCurrentTime');
     const wrapper = mount(<Game />);
-    expect(spy).toHaveBeenCalled();
-    expect(spy.calls.length).toEqual(1);
-    Game.prototype.setCurrentTime.restore();
+    expect(wrapper.state('time')).to.not.equal('undefined');
   });
 });
 
 describe('Pancake', () => {
   it('sets up the interval updating the cooking time every second', () => {
-    const spy = expect.spyOn(Pancake.prototype, 'startInterval');
     const wrapper = mount(<Pancake />);
-    expect(spy).toHaveBeenCalled();
-    expect(spy.calls.length).toEqual(1);
-    Pancake.prototype.startInterval.restore();
+    expect(wrapper.node.interval._idleTimeout).to.equal(1000);
   });
 
   it('cleans up the interval when the component is destroyed', () => {
-    const spy = expect.spyOn(Pancake.prototype, 'cleanUpInterval');
     const wrapper = mount(<Pancake />);
-    wrapper.unmount();
-    expect(spy).toHaveBeenCalled();
-    expect(spy.calls.length).toEqual(1);
-    Pancake.prototype.cleanUpInterval.restore();
+    wrapper.unmount()
+    expect(wrapper.node.interval._idleTimeout).to.equal(-1);
   });
 });
