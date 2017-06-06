3be139c46ae25e903ddedd966b97df99ecb7fa87
Luke Ghenco <lukeghenco@gmail.com>
2017-06-06 10:58:45 -0400

    Update starter JS code

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,22 +1,36 @@
 import React from 'react';
+
 import Pancake from './Pancake';
 
-export default class Game extends React.Component {
+class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: undefined,
+      pancakes: [],
+      cooked: 0,
+      burnt: 0,
+      raw: 0
+    };
+  }
 
   // TODO: create a componentWillMount() which will set the current time
 
-  setCurrentTime() {
+  setCurrentTime = () => {
     this.setState({ time: new Date(Date.now())});
   }
 
-  addPancake() {
+  addPancake = () => {
     this.setState({
       pancakes: this.state.pancakes.concat(Date.now())
     });
   }
 
-  takeItOff(id, status) {
+  takeItOff = (id, status) => {
     const { pancakes, cooked, burnt, raw } = this.state;
+
     this.setState({
       pancakes: pancakes.filter(pancake => !(pancake === id)),
       cooked: status === 'cooked' ? cooked + 1 : cooked,
@@ -25,22 +39,10 @@ export default class Game extends React.Component {
     });
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: undefined,
-      pancakes: [],
-      cooked: 0,
-      burnt: 0,
-      raw: 0
-    };
-    this.addPancake = this.addPancake.bind(this);
-    this.takeItOff = this.takeItOff.bind(this);
-  }
-
   render() {
     const { pancakes, burnt, cooked, raw, time } = this.state;
     const pans = pancakes.map((pancake, index) => <Pancake key={index} id={pancake} takeItOff={this.takeItOff} />);
+
     return (
       <div className="Game">
         <span>Pancake shop opened at: {time ? time.toString() : ''}</span>
@@ -49,9 +51,16 @@ export default class Game extends React.Component {
           <div className="Game__score --burnt">Burnt: {burnt}</div>
           <div className="Game__score --raw">Raw: {raw}</div>
         </div>
-        <button onClick={this.addPancake} className="Game__button">New pancake!</button>
+        <button 
+          onClick={this.addPancake} 
+          className="Game__button"
+        >
+          New pancake!
+        </button>
         <div className="Game__pancakes">{pans}</div>
       </div>
     )
   }
 }
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const Game = require('./components/Game');
+import Game from './components/Game';
 
 ReactDOM.render(
   <Game />,
