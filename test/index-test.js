ba8f0f33066e9c57095f8d8cfaa7d6bfc170fa73
maxwell <github email address>
2017-10-17 11:39:57 -0400

    updated tests for new Enzyme

--- test/index-test.js
+++ test/index-test.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import sinon from 'sinon';
 import { expect } from 'chai';
-import { shallow, mount } from 'enzyme';
-
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Game from '../src/components/Game';
 import Pancake from '../src/components/Pancake';
 
+Enzyme.configure({ adapter: new Adapter() })
+
+
+var clock;
+beforeEach(function () {
+
+     clock = sinon.useFakeTimers();
+ });
+
 describe('Game', () => {
   it('sets the initial time when the game was started in componentWillMount', () => {
-    const wrapper = mount(<Game />);
-    expect(wrapper.state('time')).to.not.equal('undefined');
+
+    const gWrapper = mount(<Game />);
+    expect(gWrapper.state('time')).to.not.equal('undefined');
   });
 });
 
 describe('Pancake', () => {
+
   it('sets up the interval updating the cooking time every second', () => {
-    const wrapper = mount(<Pancake />);
-    expect(wrapper.node.interval._idleTimeout).to.equal(1000);
+    const pWrapper = mount(<Pancake />);
+    clock.tick(1010);
+    pWrapper.find('button').simulate('click')
+    pWrapper.update()
+    expect(pWrapper.state('timeCooked')).to.equal(1);
   });
 
-  it('cleans up the interval when the component is destroyed', () => {
-    const wrapper = mount(<Pancake />);
-    wrapper.unmount()
-    expect(wrapper.node.interval._idleTimeout).to.equal(-1);
+  it('calls componentWillUnmount', () => {
+    const pWrapper = mount(<Pancake />);
+    pWrapper.unmount()
+    expect(pWrapper.type()).to.equal(null);
   });
 });
