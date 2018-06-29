12b858b8028ea3c6ee5ff8a273da950d17cc71f6
maxwell <github email address>
2018-06-29 10:10:00 -0400

    recreating solution with updates

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React from 'react';
 
 class Pancake extends React.Component {
+
   constructor(props) {
     super(props);
 
     this.state = {
       timeCooked: 0,
-      flippedAt: null
+      flippedAt: undefined
     };
   }
 
@@ -21,48 +22,52 @@ class Pancake extends React.Component {
   updateCounter = () => {
     this.setState({
       timeCooked: this.state.timeCooked + 1
-    });
-  };
+    })
+  }
 
   startInterval = () => {
     this.interval = setInterval(this.updateCounter, 1000);
-  };
+  }
 
   cleanUpInterval = () => {
     clearInterval(this.interval);
-  };
+  }
 
   flip = () => {
     this.setState({
       flippedAt: this.state.timeCooked
-    });
-  };
+    })
+  }
 
   getPancakeStatus = () => {
     const { timeCooked, flippedAt } = this.state;
 
     // first side
-    if (flippedAt === null && typeof flippedAt !== "number") {
-      if (timeCooked < 2) return "raw";
-      if (timeCooked === 2) return "cooked";
-      return "burnt";
+    if (!flippedAt) {
+      if (timeCooked < 2) return 'raw';
+      if (timeCooked === 2) return 'cooked';
+      return 'burnt';
     }
 
     //second side
-    if (flippedAt > 2 || timeCooked > 4) return "burnt";
-    if (timeCooked === 4 && flippedAt === 2) return "cooked";
-    return "raw";
-  };
+    if (timeCooked > 4) return 'burnt'
+    if (timeCooked === 4) return 'cooked'
+    if (timeCooked < 4) return 'raw'
+    return 'burnt'
+
+
+  }
 
   takeItOff = () => {
     const { id } = this.props;
+    const { timeCooked, flippedAt } = this.state;
     let status = this.getPancakeStatus();
     this.props.takeItOff(id, status);
-  };
+  }
 
   render() {
     const { timeCooked, flippedAt } = this.state;
-    const firstSide = Boolean(this.state.flippedAt === null && typeof flippedAt !== "number");
+    const firstSide = Boolean(this.state.flippedAt === undefined);
     const status = this.getPancakeStatus();
 
     return (
@@ -70,19 +75,18 @@ class Pancake extends React.Component {
         <div className="Pancake__content">
           <p>I am a pancake.</p>
           <p>
-            Time cooked on {`${firstSide ? "first" : "second"}`} side:{" "}
-            {`${firstSide ? timeCooked : timeCooked - flippedAt}`}
+            Time cooked on {`${firstSide ? 'first' : 'second'}`} side: {`${firstSide ? timeCooked : timeCooked - flippedAt}`}
           </p>
           <div>
-            {firstSide ? (
+            { firstSide ?
               <button onClick={this.flip}>Flip me!</button>
-            ) : (
+              :
               <button onClick={this.takeItOff}>Take me off!</button>
-            )}
+            }
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
